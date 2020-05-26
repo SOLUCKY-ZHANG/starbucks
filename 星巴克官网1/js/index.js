b2bbf31a4e0661b780b80c98870606ad0f0ef70f
SOLUCKY-ZHANG <392218997@qq.com>
2020-05-26 19:37:40 +0800

    响应式星巴克官网

---
+++ 星巴克官网1/js/index.js
@@ -0,0 +1,6 @@
+$('#showBtn').on('click',function(){
+    $('.menu').removeClass('hide').addClass('show');
+});
+$('#closeBtn').on('click',function(){
+    $('.menu').removeClass('show').addClass('hide');
+});
